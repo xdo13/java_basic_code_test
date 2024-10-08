ad2a60b76b0a122496d2931a2abe1f04dcd993e9
xdo13 <xdo13@naver.com>
2024-10-08 18:03:04 +0900

    10월 8일 실습

---
+++ src/i/Test84.java
@@ -0,0 +1,10 @@
+package i;
+
+public class Test84 {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        System.out.println("a = " + a);
+        a=null;
+        System.out.println("a = " + a);
+    }
+}
---
+++ src/i/Test88.java
@@ -0,0 +1,4 @@
+package i;
+
+public class Test88 {
+}
