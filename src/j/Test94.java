227930e05ab2aabe18c77f7dd252e0e753373d85
xdo13 <xdo13@naver.com>
2024-10-10 17:45:01 +0900

    배열 및 함수 등 실습

---
+++ src/j/Test94.java
@@ -0,0 +1,10 @@
+package j;
+
+public class Test94 {
+    public static void main(String[] args) {
+        int[] a= new int[5];
+        System.out.println("a= " + a);
+        a = null;
+        System.out.println("a= " + a);
+    }
+}
---
+++ src/j/Test99.java
@@ -0,0 +1,27 @@
+package j;
+
+import java.util.Scanner;
+
+class Test99 {
+    static  int sign0f(int n) {
+        int sign = 0;
+
+        if (n > 0)
+            sign = 1;
+        else if (n < 0)
+            sign = -1;
+
+        return sign;
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("정수 x: ");
+        int x = stdIn.nextInt();
+
+        int s = sign0f(x);
+        System.out.println("sing0f(x)는 " +s +"입니다.");
+    }
+
+}
