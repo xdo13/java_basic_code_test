bc04d58aca63c584b3db481eaf290d314707d602
xdo13 <xdo13@naver.com>
2024-10-14 17:53:21 +0900

    배열 및 다중 정의 메서드 실습

---
+++ src/l/Test119.java
@@ -0,0 +1,37 @@
+package l;
+
+import java.util.Scanner;
+
+public class Test119 {
+    static void printBits(byte x){
+        for (int i = 7; i >= 0; i--)
+            System.out.print(((x >>> i &1)==1) ? '1' : '0');
+    }
+    static void printBits (short x){
+        for (int i = 15; i >= 0; i--)
+            System.out.print(((x>>>i&1) == 1)? '1' : '0');
+    }
+    static void printBits (int x){
+        for (int i = 31; i >= 0; i--)
+            System.out.print(((x>>>i&1) == 1)? '1' : '0');
+    }
+    static void printBits (long x){
+        for (int i = 63; i >= 0; i--)
+            System.out.print(((x>>>i&1) == 1)? '1' : '0');
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("byte 형 정수 a의 값:"); byte a = stdIn.nextByte();
+        System.out.print("short 형 정수 b의 값:"); short b = stdIn.nextShort();
+        System.out.print("int 형 정수 c의 값:"); int c = stdIn.nextInt();
+        System.out.print("long 형 정수 d의 값:"); long d = stdIn.nextLong();
+
+        System.out.print("a의 비트: "); printBits(a);
+        System.out.print("\nb의 비트: "); printBits(b);
+        System.out.print("\nc의 비트: "); printBits(c);
+        System.out.print("\nd의 비트: "); printBits(d);
+        System.out.println();
+    }
+}
