1837884cb953d053646af2b4091c372cf9d6a001
xdo13 <xdo13@naver.com>
2024-10-24 17:50:19 +0900

    가위바위보 프로그램

---
+++ src/t/exam03/FingerFlashing3.java
@@ -0,0 +1,51 @@
+package t.exam03;
+
+import r.ComputerPlayer;
+import r.HumanPlayer;
+
+import java.util.Scanner;
+
+class FingerFlashing3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HumanPlayer hp = new HumanPlayer();
+        ComputerPlayer cp1 = new ComputerPlayer();
+        ComputerPlayer cp2 = new ComputerPlayer();
+
+        String[] hands = {"가위", "바위", "보"};
+        int retry;
+
+        do{
+            int comp1 = cp1.nextHand();
+            int comp2 = cp2.nextHand();
+
+            int user= hp.nextHand();
+
+            System.out.println("컴퓨터1은 " +hands[comp1] + "이고, "+
+                    "컴퓨터2는" +hands[comp2] + "이며, " + "당신은" + hands[user] + "입니다");
+
+            int r1 = (user - comp1 +3) % 3;
+            int r2 = (user - comp2 +3) % 3;
+
+            if (r1 == 2 && r2 == 2)
+                System.out.println("당신이 이겼습니다.");
+            else if (r1 == 1 && r2 == 0)
+                System.out.println("컴퓨터1이 이겼습니다.");
+            else if (r1 == 0 && r2 == 1)
+                System.out.println("컴퓨터2이 이겼습니다.");
+            else if(r1 == 0 && r2 == 2)
+                System.out.println("당신과 컴퓨터 1이 이겼습니다");
+            else if(r1 == 2 && r2 == 0)
+                System.out.println("당신과 컴퓨터 2가 이겼습니다");
+            else if(r1 == 1 && r2 == 1)
+                System.out.println("컴퓨터1과 2의 승리입니다.");
+            else
+                System.out.println("비겼습니다");
+            do {
+                System.out.print("다시 한 번? (0) 아니오 (1) 네:");
+                retry = scanner.nextInt();
+            }while (retry != 0 && retry != 1);
+        }while (retry ==1);
+
+    }
+}
